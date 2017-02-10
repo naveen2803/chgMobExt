147d0c68455d55d352a154dee588ae0528ddb04d
naveenm <naveen.malhotra@intelligentpathways.com.au>
2017-02-10 10:39:15 +1000

    updated

--- index.js
+++ index.js
@@ -33,7 +33,36 @@ app.get('/', function (req, res) {
   ].join('\n');
 
   res.header("Content-Type", 'application/json');
-  res.status(200).send({"name":"naveen"});
+  res.status(200).send({"hello":"world"});
+});
+
+app.get('/:userid', function (req, res) {
+    var userId = req.params.userid;
+  var view = [
+    '<html>',
+    '  <head>',
+    '    <title>Auth0 Extension</title>',
+    '    <script type="text/javascript">',
+    '       if (!sessionStorage.getItem("token")) {',
+    '         window.location.href = "'+res.locals.baseUrl+'/login";',
+    '       }',
+    '    </script>',
+    '  </head>',
+    '  <body>',
+    '    <p><strong>Token</strong></p>',
+    '    <textarea rows="10" cols="100" id="token"></textarea>',
+    '    <script type="text/javascript">',
+    '       var token = sessionStorage.getItem("token");',
+    '       if (token) {',
+    '         document.getElementById("token").innerText = token;',
+    '       }',
+    '    </script>',
+    '  </body>',
+    '</html>'
+  ].join('\n');
+
+  res.header("Content-Type", 'application/json');
+  res.status(200).send({"userid": userId});
 });
 
 // This endpoint would be called by webtask-gallery to dicover your metadata
